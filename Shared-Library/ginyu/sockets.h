000b4d6b31dde1ee2e5252962306a716b9cfce83
MaximilianoFelice <maximilianofelice@gmail.com>
2013-11-22 20:35:01 -0300

    Modificado un include que no permitia compilar

--- Shared-Library/ginyu/sockets.h
+++ Shared-Library/ginyu/sockets.h
@@ -12,7 +12,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#include <commons/log.h>
+#include "log.h" //La commons referencia a sus archivos por localidad
 #include <errno.h>
 #include <stdlib.h>
 #include <curses.h>
