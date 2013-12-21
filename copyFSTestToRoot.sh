86668248333633cb91ad938d91ca096a1ff6d6e5
MaximilianoFelice <maximilianofelice@gmail.com>
2013-12-20 23:05:25 -0300

    Agregados scripts para ejecucion en server. Acomodado el path de plataforma al script correspondiente.

--- copyFSTestToRoot.sh
+++ copyFSTestToRoot.sh
@@ -4,6 +4,6 @@
 echo "Copiando archivos... "
 cp FileSystem/scripts\ y\ utilitarios/mfc ~/mfc
 cp FileSystem/scripts\ y\ utilitarios/koopa ~/koopa
-cp FileSystem/scripts\ y\ utilitarios/Koopa\ Scripts/script-esquema1.sh ~/script-esquema1.sh
-cp FileSystem/scripts\ y\ utilitarios/Koopa\ Scripts/script1.sh ~/script1.sh
+cp FileSystem/scripts\ y\ utilitarios/Koopa\ Scripts/script-esquema1-server.sh ~/script-esquema1.sh
+cp FileSystem/scripts\ y\ utilitarios/Koopa\ Scripts/script1-server.sh ~/script1.sh
 echo "DONE!"
